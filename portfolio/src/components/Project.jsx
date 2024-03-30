99eb12afe624ddf4b3d39327abe5a9e966bb8357
Lotanna <tannaloamobi@gmail.com>
2024-03-30 16:35:55 +0000

    updating files from local

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import projects from "../datasets/projectData.json"
 
 function Project() {
   const { projectKey } = useParams();
--- portfolio/src/components/ProjectGallery.jsx
+++ portfolio/src/components/ProjectGallery.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Project from "./Project";
 
 function ProjectGallery() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
+    const jsonUrl = "/datasets/projectData.json";
+    console.log("Fetching:", jsonUrl);
     // Fetch project data from JSON file
-    fetch("/projectData.json")
-      .then((response) => response.json())
-      .then((data) => setProjects(data))
+    fetch("jsonUrl")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch: ${response.status} ${response.statusText}`
+          );
+        }
+        return response.json();
+      })
+      .then((data) => setProjects(data.projects))
       .catch((error) => console.error("Error fetching projects:", error));
   }, []);
 
@@ -21,10 +31,16 @@ function ProjectGallery() {
           {project.image && (
             <img src={`./images/${project.image}`} alt={project.title} />
           )}
+          {/* {project.image && (
+            <img
+              src={process.env.PUBLIC_URL + project.image}
+              alt={project.title}
+            />
+          )} */}
           <h5>{project.title}</h5>
           <p>{project.description}</p>
           {/* Link to project details page */}
-          <Link to={`/projects/${project.key}`}>See More</Link>
+          {/* <Link to={`/projects/${project.key}`}>See More</Link> */}
           {project.app && (
             <a href={project.app} target="_blank" rel="noopener noreferrer">
               View App
@@ -35,6 +51,9 @@ function ProjectGallery() {
               GitHub Repository
             </a>
           )}
+          <Routes>
+            <Route path="/:projectKey" element={<Project />} />
+          </Routes>
         </div>
       ))}
     </div>
